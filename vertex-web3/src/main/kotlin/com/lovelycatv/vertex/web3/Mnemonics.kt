5c3d1352930119827b4d6540137dea4bcd2c8214
LovelyCat <1252251361@qq.com>
2024-11-30 00:37:52 +0800

    feat: seed generation

--- vertex-web3/src/main/kotlin/com/lovelycatv/vertex/web3/Mnemonics.kt
+++ vertex-web3/src/main/kotlin/com/lovelycatv/vertex/web3/Mnemonics.kt
@@ -1,16 +1,21 @@
 package com.lovelycatv.vertex.web3
 
 import com.lovelycatv.vertex.web3.bip39.IWordListBIP39
+import com.lovelycatv.vertex.web3.bip39.MnemonicBIP39
 import com.lovelycatv.vertex.web3.bip39.WordListEnBIP39
 
 open class Mnemonics {
     protected val words: MutableList<IWordListBIP39>
+    private val eg: IWordListBIP39
 
     constructor(mnemonics: List<IWordListBIP39>) {
+        require(mnemonics.isNotEmpty())
+        this.eg = mnemonics[0]
         this.words = mnemonics.toMutableList()
     }
 
     constructor(mnemonicString: String, separator: String = " ", eg: IWordListBIP39 = WordListEnBIP39.ABANDON) {
+        this.eg = eg
         this.words = mnemonicString.split(separator).map { word ->
             eg.allWords()[eg.fromWordToIndex(word)]
         }.toMutableList()
@@ -18,12 +23,34 @@ open class Mnemonics {
 
     val length: Int get() = this.words.size
 
+    val entropy: MnemonicBIP39.ENTROPY get() = MnemonicBIP39.ENTROPY.fromWordCount(this.length)
+
+    fun toIndexes(): List<Int> {
+        return this.words.map { eg.fromWordToIndex(it.getWord()) }
+    }
+
+    fun toIndexesBits(): String {
+        return toIndexes().joinToString(separator = "") { it.toString(2).padStart(11, '0') }
+    }
+
+    fun toRandByteArray(): ByteArray {
+        return ByteArray(this.entropy.length / 8) {
+            this.toRandomBitsAndChecksumBits().first.chunked(8)[it].toUByte(2).toByte()
+        }
+    }
+
+    fun toRandomBitsAndChecksumBits(): Pair<String, String> {
+        return toIndexesBits().run {
+            this.dropLast(entropy.checksumBitCount) to this.drop(entropy.length)
+        }
+    }
+
     operator fun get(index: Int): IWordListBIP39 {
         return this.words[index]
     }
 
     override fun toString(): String {
-        return this.words.joinToString(" ")
+        return this.words.joinToString(" ").lowercase()
     }
 
     fun toMutable(): MutableMnemonics = MutableMnemonics(this.words)
--- vertex-web3/src/main/kotlin/com/lovelycatv/vertex/web3/bip39/MnemonicBIP39.kt
+++ vertex-web3/src/main/kotlin/com/lovelycatv/vertex/web3/bip39/MnemonicBIP39.kt
@@ -4,6 +4,8 @@ import com.alibaba.fastjson2.JSON
 import com.lovelycatv.vertex.web3.Mnemonics
 import java.security.MessageDigest
 import java.security.SecureRandom
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
 
 /**
  * @author lovelycat
@@ -16,23 +18,60 @@ object MnemonicBIP39 {
         ENT_160(160),
         ENT_192(192),
         ENT_224(224),
-        ENT_256(256)
+        ENT_256(256);
+
+        val checksumBitCount: Int get() = (this.length / 32)
+
+        val indexesBitCount: Int get() = this.length + this.checksumBitCount
+
+        val indexBitCount: Int get() = indexesBitCount / 11
+
+        companion object {
+            fun fromWordCount(count: Int): ENTROPY {
+                return when (count) {
+                    12 -> ENT_128
+                    15 -> ENT_160
+                    18 -> ENT_192
+                    21 -> ENT_224
+                    25 -> ENT_256
+                    else -> throw IllegalArgumentException("Word count must be 12 / 15 / 18 / 21 / 24")
+                }
+            }
+        }
     }
 
-    private fun generateEntropy(entLength: ENTROPY): ByteArray {
+    /**
+     * Generate a random byte array
+     *
+     * @param entLength ENTROPY
+     * @return Random ByteArray
+     */
+    fun generateEntropy(entLength: ENTROPY): ByteArray {
         val byteLength = entLength.length / 8
         val random = SecureRandom()
-        return ByteArray(byteLength).also { random.nextBytes(it) }.also { println(JSON.toJSONString(it)) }
+        return ByteArray(byteLength).also { random.nextBytes(it) }
     }
 
-    private fun calculateChecksum(entropy: ByteArray): Int {
+    /**
+     * Calculate checksum number according to the entropy byte array
+     *
+     * @param entropy Entropy ByteArray
+     * @return Checksum
+     */
+    fun calculateChecksum(entropy: ByteArray): Int {
         val hash = MessageDigest.getInstance("SHA-256").digest(entropy)
         val checksumBits = entropy.size * 8 / 32
         val checksum = hash[0].toInt() and 0xFF
         return checksum.shr(8 - checksumBits)
     }
 
-    private fun combineEntropyAndChecksum(entropy: ByteArray, checksum: Int): String {
+    /**
+     * Combine entropy and checksum to a single bits string
+     *
+     * @param entropy Entropy ByteArray
+     * @param checksum Checksum
+     */
+    fun combineEntropyAndChecksum(entropy: ByteArray, checksum: Int): String {
         val entropyBits = entropy.joinToString("") {
             it.toUByte().toString(2).padStart(8, '0')
         }
@@ -40,19 +79,44 @@ object MnemonicBIP39 {
         return entropyBits + checksumBits
     }
 
-    private fun splitBitsToIndices(bits: String): List<Int> {
+    fun splitBitsToIndices(bits: String): List<Int> {
         return bits.chunked(11).map { it.toInt(2) }
     }
 
-    private fun indicesToMnemonic(indices: List<Int>): Mnemonics {
+    fun indicesToMnemonic(indices: List<Int>): Mnemonics {
         return Mnemonics(indices.map { WordListEnBIP39.entries[it] })
     }
 
+    /**
+     * Generate a random [Mnemonics]
+     *
+     * @param entLength ENTROPY
+     * @return [Mnemonics]
+     */
     fun generateMnemonic(entLength: ENTROPY): Mnemonics {
         val entropy = generateEntropy(entLength)
         val checksum = calculateChecksum(entropy)
         val combinedBits = combineEntropyAndChecksum(entropy, checksum)
         val indices = splitBitsToIndices(combinedBits)
         return indicesToMnemonic(indices)
+    }
+
+    /**
+     * Generate bip39 seed,
+     * Eg: network drama crash paper hamster among spot when vendor slab web group (No Password),
+     * Seed: 2d3ecb5f775e788ff70e11d2690f0036faea3a80dfb3e22aeda345371fc3d767a4daec80eb8e01ce6401e656dec841cc4315909daf3731ad5dedf8064f03bdec
+     *
+     * @param mnemonics Mnemonics
+     * @param password String Default empty
+     * @return BIP39 Seed
+     */
+    fun generateSeed(mnemonics: Mnemonics, password: String = ""): ByteArray {
+        val salt = "mnemonic$password".toByteArray()
+        val iterations = 2048
+        val keyLength = 512
+
+        val pbkdf2 = PBEKeySpec(mnemonics.toString().toCharArray(), salt, iterations, keyLength)
+        val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+        return secretKeyFactory.generateSecret(pbkdf2).encoded
     }
 }
